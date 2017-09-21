ae8778f59582956d0e609c84e609004e554b2f33
AmrAnwar <amranwar945@gmail.com>
2017-09-21 14:25:30 +0200

    add more movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -14,6 +14,15 @@ def create_movies():
     Movie(title="Kimi no na Wa(your name)",
           image_url="https://images-na.ssl-images-amazon.com/images/M/MV5BODRmZDVmNzUtZDA4ZC00NjhkLWI2M2UtN2M0ZDIzNDcxYThjL2ltYWdlXkEyXkFqcGdeQXVyNTk0MzMzODA@._V1_UX182_CR0,0,182,268_AL_.jpg",
           youtube_url="https://www.youtube.com/watch?v=hRfHcp2GjVI")
+    Movie(title="A Silent Voice",
+          image_url="https://images-na.ssl-images-amazon.com/images/M/MV5BM2Y1YmQ5YzItNDIxMi00YTAxLThkMTctNTg3M2EwOTg0NWQwXkEyXkFqcGdeQXVyMjI5MjU5OTI@._V1_UY268_CR4,0,182,268_AL_.jpg",
+          youtube_url="https://www.youtube.com/watch?v=nfK6UgLra7g")
+    Movie(title="spirited away",
+          image_url="https://images-na.ssl-images-amazon.com/images/M/MV5BOGJjNzZmMmUtMjljNC00ZjU5LWJiODQtZmEzZTU0MjBlNzgxL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+          youtube_url="https://www.youtube.com/watch?v=ByXuk9QqQkk",)
+    Movie(title="the wind Rises",
+          image_url="https://images-na.ssl-images-amazon.com/images/M/MV5BMTU4NDg0MzkzNV5BMl5BanBnXkFtZTgwODA3Mzc1MDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+          youtube_url="https://www.youtube.com/watch?v=2QFBZgAZx7g",)
 
     open_movies_page(Movie.movies)
 
